f438809582bb99292365e5c87b66128060828d14
mdsams <tabrejmdsams786@gmail.com>
2022-12-12 12:00:33 +0530

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  semi: true,
+  printWidth: 100,
+};
