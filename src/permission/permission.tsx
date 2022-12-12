ec85fd242ac49cd5a10036e74ed7876911f00172
mdsams <tabrejmdsams786@gmail.com>
2022-12-12 20:50:46 +0530

    trying to implement gyroscope

--- src/permission/permission.tsx
+++ src/permission/permission.tsx
@@ -8,22 +8,28 @@ import {
 } from 'react-native-sensors';
 
 setUpdateIntervalForType(SensorTypes.accelerometer, 400); // defaults to 100ms
+setUpdateIntervalForType(SensorTypes.gyroscope, 400); // defaults to 100ms
 
 const Permission = () => {
   const [accelerometerData, setaccelerometerData] = useState<any>();
 
   useEffect(() => {
-    const subscription = setInterval(() => {
-      accelerometer.subscribe(({ x, y, z, timestamp }) => {
+    const interval = setInterval(() => {
+      const subscription = accelerometer.subscribe(({ x, y, z, timestamp }) => {
         console.log({ x, y, z, timestamp }), setaccelerometerData(timestamp);
       });
-    }, 50000);
-
-    // setTimeout(() => {
-    //   // If it's the last subscription to accelerometer it will stop polling in the native API
-    //   subscription.unsubscribe();
-    // }, 1000);
-  }, [accelerometerData]);
+      const gyrosubscription = gyroscope.subscribe(({ x, y, z, timestamp }) => {
+        console.log('from gyro');
+        console.log({ x, y, z, timestamp });
+      });
+      setTimeout(() => {
+        // If it's the last subscription to accelerometer it will stop polling in the native API
+        subscription.unsubscribe();
+        gyrosubscription.unsubscribe();
+      }, 1000);
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <View>
