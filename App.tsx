6a49943d9e7f0c1118d426bac3d5f7d71c545b19
mdghazanfar <ghazanfaralam642786@gmail.com>
2022-12-16 23:29:39 +0530

    Login page

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React, { type PropsWithChildren } from 'react';
 import { Text, useColorScheme } from 'react-native';
 import Permission from './src/permission/permission';
+import LogIn from './src/Screen/AuthPage/LogIn';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <>
-      <Text>sams</Text>
-      <Permission />
+      <LogIn />
     </>
   );
 };
